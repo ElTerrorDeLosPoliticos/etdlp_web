4c12f52b66e3128eb5a4c22453cb074854351126
AlonsoGuevara15 <alonso@itic.com.pe>
2022-04-27 18:11:57 -0500

    fix templates

--- etdlp_app/views.py
+++ etdlp_app/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def main_page(request):
-    return render(request, 'template.html', {})
+    return render(request, 'index.html', {})
